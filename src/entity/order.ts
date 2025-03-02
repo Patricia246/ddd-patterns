e32911557ac8a43d2638cd452b4fa34a0cdfa6be
Patrícia Mollica <patricia_araujo246@hotmail.com>
2025-03-02 20:07:20 -0300

    Adicionar total

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,12 +1,32 @@
 import OrderItem from './order_item';
 export default class Order {
-    _id: string;
-    _customerId: string;
-    _items: OrderItem[];
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[];
+    private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
-        this._items = [];
+        this._items = items;
+        this._total = this.total();
+        this.validate();
+    }
+
+    validate(): boolean {
+        if (!this._id) {
+            throw new Error("Invalid id");
+        }
+        if (!this._customerId) {
+            throw new Error("Invalid customerId");
+        }
+        if (this._items.length === 0) {
+            throw new Error("Items are required");
+        }
+        return true;
+    }
+
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item._price, 0);
     }
 }
