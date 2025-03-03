887d6cf57007c35a6814e4ce96bab9833f96b953
Patrícia Mollica <patricia_araujo246@hotmail.com>
2025-03-03 20:56:05 -0300

    Fix logica de price em order_item

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -15,16 +15,20 @@ describe("Order unit tests", () => {
     });
 
     it("should calculate total", () => {
-        const item1 =  new OrderItem("i1", "product", 100);
-        const item2 =  new OrderItem("i2", "product", 200);
+        const item1 =  new OrderItem("i1", "prod1", 100, "p1", 2);
+        const item2 =  new OrderItem("i2", "prod2", 200, "p1", 2);
         const order1 = new Order("o1", "c1", [item1]);
         const order2 = new Order("o1", "c1", [item1, item2]);
 
         let total1 = order1.total();
         let total2 = order2.total();
 
-        expect(total1).toBe(100);
-        expect(total2).toBe(300);
+        expect(total1).toBe(200);
+        expect(total2).toBe(600);
     });
+
+    it("should check if the item quantity is greater or equal than 0", () => {
+        expect(() => new OrderItem("i1", "prod1", 100, "p1", 0)).toThrowError("Quantity must be greater than 0");
+    }); 
 
 });
