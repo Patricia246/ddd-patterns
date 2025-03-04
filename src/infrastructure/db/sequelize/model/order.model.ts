638bb3386b2f4648dfe752fe7720de5bd884362d
Patrícia Mollica <patricia_araujo246@hotmail.com>
2025-03-04 19:41:44 -0300

    Implementando repositorio de ordem e criação do teste unitarios de create

--- src/infrastructure/db/sequelize/model/order.model.ts
+++ src/infrastructure/db/sequelize/model/order.model.ts
@@ -1,28 +1,34 @@
-import { BelongsTo, Column, ForeignKey, HasMany, Model, PrimaryKey, Table } from 'sequelize-typescript';
-import CustomerModel from './customer.model';
-import OrderItemModel from './order-item.model';
-
-@Table({
+import { 
+    Table,
+    Model,
+    PrimaryKey,
+    Column,
+    ForeignKey,
+    BelongsTo,
+    HasMany,
+  } from 'sequelize-typescript';
+  import CustomerModel from './customer.model';
+  import OrderItemModel from './order-item.model';
+  
+  @Table({
     tableName: "orders",
-    timestamps: false
-})
-export default class OrderModel extends Model {
+    timestamps: false,
+  })
+  export default class OrderModel extends Model {
     @PrimaryKey
     @Column
     declare id: string;
-
+  
     @ForeignKey(() => CustomerModel)
     @Column({ allowNull: false })
-    declare customer_id: string;;
-
+    declare customer_id: string;
+  
     @BelongsTo(() => CustomerModel)
     declare customer: CustomerModel;
-
-    @HasMany(() => OrderItemModel)
-    declare items: OrderItemModel;
-
+  
+    @HasMany(() => (require('./order-item.model').default))
+    declare items: OrderItemModel[];
+  
     @Column({ allowNull: false })
     declare total: number;
-
-
-}
+  }
