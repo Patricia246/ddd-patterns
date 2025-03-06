b6839fdf431e9252fde7f20e6340ce7a2c88ce58
Patrícia Mollica <patricia_araujo246@hotmail.com>
2025-03-05 23:14:57 -0300

    CRUD Event dispatcher

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,46 @@
+import EventDispatcher from "./event-dispatcher";
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+
+describe("Domain events tests", () => {
+    it("should register an event handler", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(
+            eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+        ).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toBe(eventHandler);
+    });
+
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+
+        expect(
+            eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+        ).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
+    })
+
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        eventDispatcher.unregisterAll();
+
+        expect(
+            eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+        ).toBeUndefined;
+        
+    });
+});
